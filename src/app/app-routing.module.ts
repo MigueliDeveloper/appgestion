42b57d4792033135dfa9f033a805ca329f1a3942
MigueliDeveloper <miguelidevelop@gmail.com>
2023-09-26 16:37:48 +0100

    holi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importar Componentes
+import { HomeComponent } from './componentes/home/home.component'
+import { CrearCursosComponent } from './componentes/crear-cursos/crear-cursos.component';
+import { ListarCursosComponent } from './componentes/listar-cursos/listar-cursos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component'
+import { CursoComponent} from './componentes/curso/curso.component'
+
+
+const routes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path: 'crearcurso', component: CrearCursosComponent},
+  {path: 'listarcursos', component: ListarCursosComponent},
+  {path: 'editarCurso/:id', component: CrearCursosComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'verCurso/:id', component: CursoComponent},
+
+  {path: '**', redirectTo: '',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Componentes
+import { HeaderComponent } from './header/header.component';
+import { ListarCursosComponent } from './componentes/listar-cursos/listar-cursos.component';
+import { CrearCursosComponent } from './componentes/crear-cursos/crear-cursos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { CursoComponent } from './componentes/curso/curso.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListarCursosComponent,
+    CrearCursosComponent,
+    HomeComponent,
+    ContactoComponent,
+    CursoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    AppRoutingModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
